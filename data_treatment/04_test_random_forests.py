5cd45a74a6ac489c63c7f28939b271c30980e4b6
Didier Dias <didierdias@Didiers-MBP.home>
2019-03-20 01:19:07 +0000

    Improved classification by removing some unnecessary features. Added a plotting of the feature importance.

--- data_treatment/04_test_random_forests.py
+++ data_treatment/04_test_random_forests.py
@@ -1,11 +1,14 @@
 import sys
 import getopt
 import pandas as pd
+import matplotlib.pyplot as plt
 from os import listdir
+from os.path import basename
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
 
 # Set variables and defaults
@@ -48,7 +51,7 @@ for file in files:
         data = pd.read_csv(file)
 
     # After some tests these seem to be the best params
-    clf = classifier = RandomForestClassifier(
+    clf = RandomForestClassifier(
         n_estimators=1200, n_jobs=5, min_samples_leaf=2, oob_score=True)
 
     data = profiles.merge(data, how="inner", left_on=[
@@ -57,11 +60,31 @@ for file in files:
     y = data.cwrb_reference_soil_group
     X = data[data.columns.values[3:]]
 
+    X = X.drop(columns=list(
+        X.loc[:, X.columns.str.contains('longitude')]))
+    X = X.drop(columns=list(
+        X.loc[:, X.columns.str.contains('latitude')]))
+    X = X.drop(columns=list(
+        X.loc[:, X.columns.str.contains('profile_layer_id')]))
+
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=42)
 
-    print(cross_val_score(clf, X_train, y_train, cv=10, n_jobs=-1))
+    clf.fit(X_train, y_train)
 
-    # 50 average 0,555180235
-    # 25 average 0,557494699
-    # 10 average 0,55182848
+    preds = clf.predict(X_test)
+    print(accuracy_score(y_test, preds))
+
+    df_ = pd.DataFrame(X.columns, columns=['feature'])
+    df_['fscore'] = clf.feature_importances_[:, ]
+
+    df_.sort_values('fscore', ascending=False, inplace=True)
+    df_ = df_[0:30]
+    df_.sort_values('fscore', ascending=True, inplace=True)
+    df_.plot(kind='barh', x='feature', y='fscore',
+             color='blue', legend=False, figsize=(12, 6))
+    plt.title('Random forest feature importance', fontsize=24)
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.savefig('feature_importance_{}.png'.format(basename(file)))
