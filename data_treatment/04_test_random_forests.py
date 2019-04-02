0530688cf63655734568a057b82ce74d263a0e50
Didier Dias <didierdias@Didiers-MBP.home>
2019-04-02 16:05:53 +0100

    Added confusion matrix plotting

--- data_treatment/04_test_random_forests.py
+++ data_treatment/04_test_random_forests.py
@@ -2,13 +2,62 @@ import sys
 import getopt
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from os import listdir
-from os.path import basename
+from os.path import splitext, basename
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=False):
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(10, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(
+        accuracy, misclass))
+    plt.savefig(title)
 
 
 # Set variables and defaults
@@ -60,31 +109,37 @@ for file in files:
     y = data.cwrb_reference_soil_group
     X = data[data.columns.values[3:]]
 
-    X = X.drop(columns=list(
-        X.loc[:, X.columns.str.contains('longitude')]))
-    X = X.drop(columns=list(
-        X.loc[:, X.columns.str.contains('latitude')]))
+    # X = X.drop(columns=list(
+    #     X.loc[:, X.columns.str.contains('longitude')]))
+    # X = X.drop(columns=list(
+    #     X.loc[:, X.columns.str.contains('latitude')]))
     X = X.drop(columns=list(
         X.loc[:, X.columns.str.contains('profile_layer_id')]))
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=42)
+        X, y, test_size=0.25)
 
+    print(cross_val_score(clf, X_train, y_train, cv=4))
     clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
 
-    preds = clf.predict(X_test)
-    print(accuracy_score(y_test, preds))
+    labels = list(y.unique())
+    labels.sort()
+    cm = confusion_matrix(y_test, y_pred, labels=labels)
 
-    df_ = pd.DataFrame(X.columns, columns=['feature'])
-    df_['fscore'] = clf.feature_importances_[:, ]
+    plot_confusion_matrix(
+        cm, labels, title='confusion_matrix_{}.pdf'.format(splitext(basename(file))[0]))
+# df_ = pd.DataFrame(X.columns, columns=['feature'])
+# df_['fscore'] = clf.feature_importances_[:, ]
 
-    df_.sort_values('fscore', ascending=False, inplace=True)
-    df_ = df_[0:30]
-    df_.sort_values('fscore', ascending=True, inplace=True)
-    df_.plot(kind='barh', x='feature', y='fscore',
-             color='blue', legend=False, figsize=(12, 6))
-    plt.title('Random forest feature importance', fontsize=24)
-    plt.xlabel('')
-    plt.ylabel('')
+# df_.sort_values('fscore', ascending=False, inplace=True)
+# df_ = df_[0:30]
+# df_.sort_values('fscore', ascending=True, inplace=True)
+# df_.plot(kind='barh', x='feature', y='fscore',
+#          color='blue', legend=False, figsize=(12, 6))
+# plt.title('Random forest feature importance', fontsize=24)
+# plt.xlabel('')
+# plt.ylabel('')
 
-    plt.savefig('feature_importance_{}.png'.format(basename(file)))
+# plt.savefig('feature_importance_{}.png'.format(basename(file)))
--- data_treatment/data_treatment.py
+++ data_treatment/data_treatment.py
@@ -5,28 +5,59 @@ from fancyimpute import KNN
 from multiprocessing import Pool
 
 
-# Receives an array of layers of a profile and returns a single row
-def merge_profile_depth(layers):
+def getOverlap(a, b):
+    return max(0, min(a[1], b[1]) - max(a[0], b[0]))
+
+
+def merge_profile_weighted(layers):
+    u_depth = 0
     final_row = pd.DataFrame()
-    for depth in range(0, max_depth+1, layer_depth):
-        # Find the layer for current depth
-        row = layers.loc[(layers['lower_depth'] > depth)].head(1)
+    for l_depth in range(layer_depth, max_depth+1, layer_depth):
+        row = pd.DataFrame()
+        total_overlap = 0
 
-        # Default to the last layer
+        # Find each layer in our profile that belongs to the final layer
+        for _, l in layers.iterrows():
+            # Find the overlap layer to use as weight
+            overlap = getOverlap([l.upper_depth, l.lower_depth], [
+                u_depth, l_depth]) / layer_depth
+            if overlap != 0:
+                # Multiply by the weight and add to the row
+                total_overlap += overlap
+                if(row.empty):
+                    row = l.apply(lambda x: x * overlap)
+                else:
+                    row = row.add(l.apply(lambda x: x * overlap))
+
         if row.empty:
-            row = layers.iloc[[-1]]
+            # Default to the last layer in the profile
+            row = layers.tail(1)
+        else:
+            if total_overlap != 1:
+                # If we got to the end but there is still part of the layer to be described
+                # we use the remaining weight and multiply by the last layer
+                row = row.add(l.apply(
+                    lambda x: x * (1-total_overlap)))
 
-        # Rename the layer
-        row = row.rename(columns=lambda x: x + "_" + str(depth))
+            # Transform back to a DataFrame for easier handling
+            row = pd.DataFrame(data=[row], columns=list(layers.columns))
+
+        # Fix u/l_depth
+        row = row.assign(upper_depth=u_depth)
+        row = row.assign(lower_depth=l_depth)
 
         # Append columns
         if final_row.empty:
             final_row = row
         else:
-            final_row = final_row.merge(row, how="inner", left_on=[
-                final_row.columns.values[0]], right_on=[row.columns.values[0]])
-    # Add a columns describing the total number of layers
-    final_row['n_layers'] = len(layers)
+            # To avoid floating point errors, dont even ask
+            row = row.assign(profile_id=list(final_row['profile_id'])[0])
+            final_row = final_row.merge(
+                row, how='inner', on='profile_id', suffixes=('', '_{}'.format(str(u_depth))))
+
+        u_depth = l_depth
+
+    final_row = final_row.assign(n_layers=len(layers))
     return final_row
 
 
@@ -37,26 +68,24 @@ def merge_profile_layers(layers):
         # Find the current layer
         row = layers.iloc[[i]] if len(layers) > i else layers.tail(1)
 
-        # Rename the layer
-        row = row.rename(columns=lambda x: x + "_" + str(i))
-
         # Append columns
         if final_row.empty:
             final_row = row
         else:
-            final_row = final_row.merge(row, how="inner", left_on=[
-                final_row.columns.values[0]], right_on=[row.columns.values[0]])
+            final_row = final_row.merge(
+                row, how='inner', on='profile_id', suffixes=('', '_{}'.format(str(i))))
     # Add a columns describing the total number of layers
-    final_row['n_layers'] = len(layers)
+    final_row = final_row.assign(n_layers=len(layers))
     return final_row
 
 
 # Set variables and defaults
 input_profiles = '../data/profiles.csv'
 input_layers = '../data/layers.csv'
+input_file = ''
 outputfile = '../data/classified_data.csv'
 threshold = 0.01
-h = 'data_treatment.py -h <help> -p <profile file> -l <layers file> -t <threshold to remove columns(default 0.01)> -c <filter country> -k <impute with k neighbours> -d <merge with depth d> -m <merge by m layers> -o <outputfile>'
+h = 'data_treatment.py -h <help> -i <input file> -p <profile file> -l <layers file> -t <threshold to remove columns(default 0.01)> -c <filter country> -k <impute with k neighbours> -d <merge with depth d> -m <merge by m layers> -o <outputfile>'
 country_filter = ''
 knn = 0
 layer_depth = 0
@@ -65,7 +94,7 @@ n_layers = 0
 
 # Read arguments
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hp:l:t:o:c:k:d:m:")
+    opts, args = getopt.getopt(sys.argv[1:], "hi:p:l:t:o:c:k:d:m:")
 except getopt.GetoptError:
     print(h)
     sys.exit(2)
@@ -76,6 +105,8 @@ for opt, arg in opts:
         sys.exit()
     elif opt in ('-p'):
         input_profiles = arg
+    elif opt in ('-i'):
+        input_file = arg
     elif opt in ('-l'):
         input_layers = arg
     elif opt in ('-t'):
@@ -91,43 +122,48 @@ for opt, arg in opts:
     elif opt in ('-m'):
         n_layers = int(arg)
 
+if input_file == '':
+    profiles = pd.read_csv(input_profiles)
+    layers = pd.read_csv(input_layers)
 
-profiles = pd.read_csv(input_profiles)
-layers = pd.read_csv(input_layers)
+    print('Merging and cleaning data.')
 
-print('Merging and cleaning data.')
+    # Selecting only those classified using WRB
+    classified_profiles = profiles[~profiles['cwrb_reference_soil_group'].isnull(
+    )]
 
-# Selecting only those classified using WRB
-classified_profiles = profiles[~profiles['cwrb_reference_soil_group'].isnull()]
+    # Merging of the files
+    classified_data = layers.merge(classified_profiles, how="inner", left_on=[
+        'profile_id'], right_on=['profile_id'])
 
-# Merging of the files
-classified_data = layers.merge(classified_profiles, how="inner", left_on=[
-                               'profile_id'], right_on=['profile_id'])
+    # Filter by country
+    if country_filter != '':
+        classified_data = classified_data.loc[classified_data['country_id']
+                                              == country_filter]
 
-# Filter by country
-if country_filter != '':
-    classified_data = classified_data.loc[classified_data['country_id']
-                                          == country_filter]
+    # Replace Albeluvisols with Retisols as was made in the latest version of WRB
+    classified_data.loc[classified_data['cwrb_reference_soil_group']
+                        == 'Albeluvisols', 'cwrb_reference_soil_group'] = 'Retisols'
 
-# Replace Albeluvisols with Retisols as was made in the latest version of WRB
-classified_data.loc[classified_data['cwrb_reference_soil_group']
-                    == 'Albeluvisols', 'cwrb_reference_soil_group'] = 'Retisols'
+    # Drop columns that are not numeric / not necessary (licenses)
+    classified_data = classified_data.drop(columns=['dataset_id', 'country_id', 'cfao_version', 'cfao_major_group',
+                                                    'cwrb_version', 'cwrb_reference_soil_group', 'cstx_version', 'cstx_order_name', 'translated', 'profile_layer_id'])
+    classified_data = classified_data.drop(columns=list(
+        classified_data.loc[:, classified_data.columns.str.contains('license')]))
 
-# Drop columns that are not numeric / not necessary (licenses)
-classified_data = classified_data.drop(columns=['dataset_id', 'country_id', 'cfao_version', 'cfao_major_group',
-                                                'cwrb_version', 'cwrb_reference_soil_group', 'cstx_version', 'cstx_order_name', 'translated'])
-classified_data = classified_data.drop(columns=list(
-    classified_data.loc[:, classified_data.columns.str.contains('license')]))
+    # Remove the columns that have only missing values
+    classified_data = classified_data.dropna(axis=1, how='all')
 
-# Remove the columns that have only missing values
-classified_data = classified_data.dropna(axis=1, how='all')
+    # Remove all columns with more null values than allowed by the threshold
+    c = classified_data.count().apply(lambda x: x/classified_data.shape[0])
+    classified_data = classified_data.drop(
+        columns=c[c < threshold].index.tolist())
 
-# Remove all columns with more null values than allowed by the threshold
-c = classified_data.count().apply(lambda x: x/classified_data.shape[0])
-classified_data = classified_data.drop(columns=c[c < threshold].index.tolist())
+    print('Cleaned data has {} rows and {} columns'.format(
+        classified_data.shape[0], classified_data.shape[1]) + (', filtered by {}'.format(country_filter) if country_filter != '' else ''))
+else:
+    classified_data = pd.read_csv(input_file)
 
-print('Cleaned data has {} rows and {} columns'.format(
-    classified_data.shape[0], classified_data.shape[1]) + (', filtered by {}'.format(country_filter) if country_filter != '' else ''))
 
 # Perform imputation
 if knn != 0:
@@ -163,12 +199,15 @@ if n_layers != 0 or layer_depth != 0:
             print('Merging {} layers of {} profiles, each containing {} layers of depth {} in the end.'.format(
                 classified_data.shape[0], len(profiles), int(max_depth/layer_depth), layer_depth))
             classified_data = pd.concat(
-                pool.map(merge_profile_depth, profiles))
+                pool.map(merge_profile_weighted, profiles))
 
-    # Fix naming
-    classified_data['profile_id'] = classified_data['profile_id_0']
+    # Fix some columns
     classified_data = classified_data.drop(columns=list(
         classified_data.loc[:, classified_data.columns.str.contains('profile_id_')]))
+    classified_data = classified_data.drop(columns=list(
+        classified_data.loc[:, classified_data.columns.str.contains('latitude_')]))
+    classified_data = classified_data.drop(columns=list(
+        classified_data.loc[:, classified_data.columns.str.contains('longitude_')]))
 
 
 print('Final data has {} rows and {} columns.'.format(
