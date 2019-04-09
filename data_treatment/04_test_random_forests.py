e7bbce29c9ccef5ed84109cea2161c99ac582a23
Didier Dias <didierdias@Didiers-MBP.home>
2019-04-09 14:22:04 +0100

    Added dsm data and refined random forest

--- data_treatment/04_test_random_forests.py
+++ data_treatment/04_test_random_forests.py
@@ -10,7 +10,47 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, precision_score, recall_score, f1_score, make_scorer
+
+
+def classification_report_with_accuracy_score(y_true, y_pred):
+    print("==== REPORT OF RESULTS FOR ONE OF THE DATA FOLDS  ====")
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    print(classification_report(y_true, y_pred))
+    acc = accuracy_score(y_true, y_pred)
+    pre_micro = precision_score(y_true, y_pred, average='micro')
+    rec_micro = recall_score(y_true, y_pred, average='micro')
+    f_1_micro = f1_score(y_true, y_pred, average='micro')
+    pre_macro = precision_score(y_true, y_pred, average='macro')
+    rec_macro = recall_score(y_true, y_pred, average='macro')
+    f_1_macro = f1_score(y_true, y_pred, average='macro')
+    pre_weighted = precision_score(y_true, y_pred, average='weighted')
+    rec_weighted = recall_score(y_true, y_pred, average='weighted')
+    f_1_weighted = f1_score(y_true, y_pred, average='weighted')
+    return acc
+
+
+def remove_lat_lon(X):
+    print("Removing latitude and longitude")
+
+    X = X.drop(columns=list(
+        X.loc[:, X.columns.str.contains('longitude')]))
+    X = X.drop(columns=list(
+        X.loc[:, X.columns.str.contains('latitude')]))
+
+    return X
+
+
+def remove_small_classes(df, min):
+    uniques = df.cwrb_reference_soil_group.unique()
+    for u in uniques:
+        cnt = df[df.cwrb_reference_soil_group == u].shape[0]
+        if cnt < min:
+            df = df[df.cwrb_reference_soil_group != u]
+            print('Deleting {} with {} occurrences'.format(u, cnt))
+
+    return df
 
 
 def plot_confusion_matrix(cm,
@@ -29,7 +69,7 @@ def plot_confusion_matrix(cm,
     if cmap is None:
         cmap = plt.get_cmap('Blues')
 
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(13, 13))
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -45,7 +85,7 @@ def plot_confusion_matrix(cm,
     thresh = cm.max() / 1.5 if normalize else cm.max() / 2
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+            plt.text(j, i, "{:0.2f}".format(cm[i, j]),
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
         else:
@@ -106,40 +146,43 @@ for file in files:
     data = profiles.merge(data, how="inner", left_on=[
         'profile_id'], right_on=['profile_id'])
 
+    data = remove_small_classes(data, 15)
+
     y = data.cwrb_reference_soil_group
     X = data[data.columns.values[3:]]
 
-    # X = X.drop(columns=list(
-    #     X.loc[:, X.columns.str.contains('longitude')]))
-    # X = X.drop(columns=list(
-    #     X.loc[:, X.columns.str.contains('latitude')]))
+    # Remove unecessary columns
+    # X = remove_lat_lon(X)
     X = X.drop(columns=list(
         X.loc[:, X.columns.str.contains('profile_layer_id')]))
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25)
 
-    print(cross_val_score(clf, X_train, y_train, cv=4))
+    # , scoring=make_scorer(classification_report_with_accuracy_score))
+    res = cross_val_score(clf, X_train, y_train, cv=10)
+    print(res, 'avg: ', np.mean(res))
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    #print(accuracy_score(y_test, y_pred))
 
     labels = list(y.unique())
     labels.sort()
     cm = confusion_matrix(y_test, y_pred, labels=labels)
 
     plot_confusion_matrix(
-        cm, labels, title='confusion_matrix_{}.pdf'.format(splitext(basename(file))[0]))
-# df_ = pd.DataFrame(X.columns, columns=['feature'])
-# df_['fscore'] = clf.feature_importances_[:, ]
+        cm, labels, title='confusion_matrix_{}.pdf'.format(splitext(basename(file))[0]), normalize=True)
 
-# df_.sort_values('fscore', ascending=False, inplace=True)
-# df_ = df_[0:30]
-# df_.sort_values('fscore', ascending=True, inplace=True)
-# df_.plot(kind='barh', x='feature', y='fscore',
-#          color='blue', legend=False, figsize=(12, 6))
-# plt.title('Random forest feature importance', fontsize=24)
-# plt.xlabel('')
-# plt.ylabel('')
+    df_ = pd.DataFrame(X.columns, columns=['feature'])
+    df_['fscore'] = clf.feature_importances_[:, ]
 
-# plt.savefig('feature_importance_{}.png'.format(basename(file)))
+    df_.sort_values('fscore', ascending=False, inplace=True)
+    df_ = df_[0:30]
+    df_.sort_values('fscore', ascending=True, inplace=True)
+    df_.plot(kind='barh', x='feature', y='fscore',
+             color='blue', legend=False, figsize=(12, 6))
+    plt.title('Random forest feature importance', fontsize=24)
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.savefig('feature_importance_{}.png'.format(basename(file)))
---
+++ data_treatment/05_add_dsm_data.py
@@ -0,0 +1,98 @@
+
+import os
+import sys
+import getopt
+import numpy as np
+import pandas as pd
+import richdem as rd
+import georasters as gr
+import matplotlib.pyplot as plt
+
+
+def add_elevation(df, file, indexes):
+    try:
+        table = gr.from_file(file)
+        for index in indexes:
+            try:
+                row = df.loc[index]
+                val = table.map_pixel(row['longitude'], row['latitude'])
+                df.loc[index, 'elevation'] = float(val)
+            except:
+                df.loc[index, 'elevation'] = 0
+    except:
+        for index in indexes:
+            df.loc[index, 'elevation'] = 0
+    return df
+
+
+def add_slope_aspect_curvature(df, file, indexes):
+    for attr in ['slope_percentage', 'aspect', 'profile_curvature']:
+        table = None
+        try:
+            table = rd.TerrainAttribute(
+                rd.LoadGDAL(file, no_data=-9999), attrib=attr)
+            rd.SaveGDAL("./temp.tif", table)
+            table = None
+            table = gr.from_file("./temp.tif")
+            for index in indexes:
+                try:
+                    row = df.loc[index]
+                    val = table.map_pixel(row['longitude'], row['latitude'])
+                    df.loc[index, attr] = float(val)
+                except:
+                    df.loc[index, attr] = 0
+            os.remove("./temp.tif")
+        except:
+            for index in indexes:
+                df.loc[index, attr] = 0
+    return df
+
+
+# Set variables and defaults
+inputfile = '../data/data.csv'
+outputfile = '../data/data_with_dsm.csv'
+dsm_folder = '../data/rasters'
+
+h = '05_add_dsm_data.py -h <help> -i <inputfile> -o <outputfile> -d <dsm files folder>'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:d:")
+except getopt.GetoptError:
+    print(h)
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print(h)
+        sys.exit()
+    elif opt in ('-i'):
+        inputfile = arg
+    elif opt in ('-o'):
+        outputfile = arg
+    elif opt in ('-d'):
+        dsm_folder = arg
+
+
+df = pd.read_csv(inputfile)
+needed_files = {}
+
+# Fill the needed files for generating terrain data
+for index, row in df.iterrows():
+    key = '{}/N{:03}W{:03}_AVE_DSM.tif'.format(
+        dsm_folder, abs(int(row['latitude'])), abs(int(row['longitude'])))
+    if key not in needed_files:
+        needed_files[key] = [index]
+    else:
+        needed_files.get(key).append(index)
+
+# Fill the terrain data
+cnt = 0
+for file in needed_files.keys():
+    df = add_elevation(df, file, needed_files.get(file))
+    df = add_slope_aspect_curvature(df, file, needed_files.get(file))
+    cnt += len(needed_files.get(file))
+    print("Added terrain information to " + repr(cnt) +
+          " instances out of " + repr(len(df)) + " data instances...")
+
+
+df.to_csv(outputfile, index=False)
